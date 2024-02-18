8927db968bc8206c761dc008fcf6f23e350d0395
PontusBengtsson <pontus.bengtsson@live.com>
2024-02-18 21:28:00 +0100

    Lektion 3

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-import express from "express";
+import { getProducts } from './services/api.js';
+import express from 'express';
 const app = express();
-import {config} from "dotenv";
+import { config } from 'dotenv';
 config();
 
-
-
-app.get("/", async (request, response) => {
-    const markup = "<h1>Hello Klarna</h1>";
-    response.send(markup);
+app.get('/', async (req, res) => {
+	const products = await getProducts();
+	const markup = products
+		.map(
+			(p) =>
+				`<a style="display:block;color:black;border:solid black 2px;margin: 20px; padding:10px;" href="/product/${p.id}">
+                    ${p.title} - ${p.price}kr
+                </a>`
+		)
+		.join(' ');
+	res.send(markup);
 });
 
-app.get("/products/:id", async (request, response) => {
-    const { id } = request.params;
-    const markup = `<h1> Hello ${id}</h1>`
-    response.send(markup);
+app.get('/product/:id', async function (req, res) {
+	const { id } = req.params;
+	const markup = `<h1>Hello ${id}</h1>`;
+	res.send(markup);
 });
 
-app.get("/confirmation", async (request, response) => {
-    const markup = `<h1> Hello Confirmation</h1>`
-    response.send(markup);
+app.get('/confirmation', async function (req, res) {
+	const markup = `<h1>Hello confirmation</h1>`;
+	res.send(markup);
 });
-
-
-
 
 app.listen(process.env.PORT);
